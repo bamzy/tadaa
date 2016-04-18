e7968116d9159969e11f26a443f4b287591dc817
Bamdad A <bamdad.ag@gmail.com>
2016-04-18 13:24:23 -0600

    added detail functionality of each task

--- src/AppBundle/Controller/ToDoController.php
+++ src/AppBundle/Controller/ToDoController.php
@@ -6,6 +6,14 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
 class ToDoController extends Controller
 {
     /**
@@ -26,8 +34,45 @@ class ToDoController extends Controller
      */
     public function createAction(Request $request)
     {
+        $task = new Task();
+        $form = $this->createFormBuilder($task)
+            ->add('name',TextType::class, array('attr'=>array('class'=>'form-control', 'style' => 'margin-buttom:15px')))
+            ->add('type',TextType::class, array('attr'=>array('class'=>'form-control', 'style' => 'margin-buttom:15px')))
+            ->add('description',TextareaType::class, array('attr'=>array('class'=>'form-control', 'style' => 'margin-buttom:15px')))
+            ->add('priority',ChoiceType::class, array('choices'=> array('Low'=>'1','Normal'=>'2','High'=>'3'),'attr'=>array('class'=>'form-control', 'style' => 'margin-buttom:15px')))
+            ->add('dueDate',DateType::class, array('attr'=>array('class'=>'form-ontrol', 'style' => 'margin-buttom:15px')))
+            ->add('Save',SubmitType::class, array('attr'=>array('class'=>'btn btn-primary btn-lg', 'style' => 'margin-top:15px')))
+            ->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+//            die('submitted');
+            $name = $form['name']->getData();
+            $type = $form['type']->getData();
+            $priority = $form['priority']->getData();
+            $dueDate = $form['dueDate']->getData();
+            $description = $form['description']->getData();
+            $creationDate = new \DateTime('now');
+
+
+            $task->setCreationDate($creationDate);
+            $task->setDueDate($dueDate);
+            $task->setName($name);
+            $task->setType($type);
+            $task->setPriority($priority);
+            $task->setDescription($description);
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em -> persist($task);
+            $em->flush();
+            $this->addFlash('notice','Task Added');
+            return $this->redirectToRoute('todo_list');
+
+        }
         return $this->render('todo/create.html.twig', array(
-            'currentLocation' => 'create'
+            'currentLocation' => 'create',
+            'form'=> $form->createView()
         ));
     }
     /**
@@ -40,8 +85,15 @@ class ToDoController extends Controller
     /**
      * @Route("/todos/detail/{id}", name="todo_details")
      */
-    public function detailAction(Request $request)
+    public function detailAction($id)
     {
+        $task = $this->getDoctrine()
+            ->getRepository('AppBundle:Task')
+            ->find($id);
+        return $this->render('todo/detail.html.twig',array(
+            'task' => $task,
+            'currentLocation' => 'list'
+        ));
         return $this->render('todo/detail.html.twig');
     }
     /**
